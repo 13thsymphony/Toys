08d413ea74458bb01e4e3427e5a73c4a95188053
Simon Tao <13thsymphony@gmail.com>
2014-09-11 18:50:16 -0700

    Initial VS2013 project
    - Win2D nuget reference
    - Shared/WP/W MainPage setup

---
+++ PhotoStrokes.Shared/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Code-behind shared by Windows and Windows Phone projects.
+// Project-specific code is found in MainPage[foo].xaml.cs.
+
+namespace PhotoStrokes
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+#if WINDOWS_PHONE_APP
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+#endif
+        }
+    }
+}
