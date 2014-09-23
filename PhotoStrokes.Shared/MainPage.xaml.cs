f0fe8fbdb30295cd3e375fda9a255a520bba77f9
13thsymphony <13thsymphony@gmail.com>
2014-09-22 23:51:33 -0700

    Render to bitmap, v1
    Latest Win2D snap has render to bitmap capabilities, now I can finish
    the basic scenario.

--- PhotoStrokes.Shared/MainPage.xaml.cs
+++ PhotoStrokes.Shared/MainPage.xaml.cs
@@ -29,12 +29,13 @@ namespace PhotoStrokes
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private float m_strokeLength = 10.0f;
-        private float m_strokeWidth = 2.0f;
+        private float m_strokeLength = 12.0f;
+        private float m_strokeWidth = 4.0f;
         private int m_numStrokes = 100;
 
         private bool m_isResourceLoadingDone;
         private CanvasBitmap m_sourceBitmap;
+        private CanvasRenderTarget m_targetBitmap;
         private byte[] m_pixelArray;
         private uint m_pixelArrayWidth;
         private uint m_pixelArrayHeight;
@@ -77,6 +78,8 @@ namespace PhotoStrokes
 
             m_pixelArray = pixelProvider.DetachPixelData();
 
+            m_targetBitmap = new CanvasRenderTarget(sender, new Size(m_pixelArrayWidth, m_pixelArrayHeight));
+
             m_rnd = new Random();
 
             m_isResourceLoadingDone = true;
@@ -90,33 +93,38 @@ namespace PhotoStrokes
             {
                 //args.DrawingSession.DrawImage(m_sourceBitmap);
 
-                // Draw a bunch of solid color strokes.
-                // - Centered at a random point
-                // - Color is determined by the bitmap value at that point
-                // - Fixed stroke length and width
-                // - Random angle
-                for (int i = 0; i < m_numStrokes; i++)
+                using (var ds = m_targetBitmap.CreateDrawingSession())
                 {
-                    double centerXFactor = m_rnd.NextDouble();
-                    double centerYFactor = m_rnd.NextDouble();
-                    float centerX = (float)(centerXFactor * m_sourceBitmap.Size.Width);
-                    float centerY = (float)(centerYFactor * m_sourceBitmap.Size.Height);
-                    float angle = (float)(m_rnd.NextDouble() * 2 * Math.PI);
-                    Color color = getColorFromBitmapCoordinates(centerXFactor, centerYFactor);
+                    // Draw a bunch of solid color strokes.
+                    // - Centered at a random point
+                    // - Color is determined by the bitmap value at that point
+                    // - Fixed stroke length and width
+                    // - Random angle
+                    for (int i = 0; i < m_numStrokes; i++)
+                    {
+                        double centerXFactor = m_rnd.NextDouble();
+                        double centerYFactor = m_rnd.NextDouble();
+                        float centerX = (float)(centerXFactor * m_sourceBitmap.Size.Width);
+                        float centerY = (float)(centerYFactor * m_sourceBitmap.Size.Height);
+                        float angle = (float)(m_rnd.NextDouble() * 2 * Math.PI);
+                        Color color = getColorFromBitmapCoordinates(centerXFactor, centerYFactor);
 
-                    // Convert the stroke definition to bounding box for line.
-                    float x1 = centerX + (float)Math.Cos(angle) * m_strokeLength / 2;
-                    // Technically the coordinate systems don't match for Y, but it doesn't matter here.
-                    float y1 = centerY + (float)Math.Sin(angle) * m_strokeLength / 2;
-                    float x2 = 2 * centerX - x1;
-                    float y2 = 2 * centerY - y1;
+                        // Convert the stroke definition to bounding box for line.
+                        float x1 = centerX + (float)Math.Cos(angle) * m_strokeLength / 2;
+                        // Technically the coordinate systems don't match for Y, but it doesn't matter here.
+                        float y1 = centerY + (float)Math.Sin(angle) * m_strokeLength / 2;
+                        float x2 = 2 * centerX - x1;
+                        float y2 = 2 * centerY - y1;
 
-                    args.DrawingSession.DrawLine(x1, y1, x2, y2, color, m_strokeWidth);
+                        ds.DrawLine(x1, y1, x2, y2, color, m_strokeWidth);
+                    }
                 }
+
+                args.DrawingSession.DrawImage(m_targetBitmap);
             }
 
             // Render looping.
-            //mainCanvas.Invalidate();
+            mainCanvas.Invalidate();
         }
 
         /// <summary>
@@ -132,7 +140,7 @@ namespace PhotoStrokes
             uint y = (uint)(yFactor * m_pixelArrayHeight);
             uint offset = (y * m_pixelArrayWidth + x) * 4;
 
-            // We get pixel data in BGRA channel order.
+            // The pixel array is stored in BGRA channel order.
             return Color.FromArgb(m_pixelArray[offset + 3], m_pixelArray[offset + 2], m_pixelArray[offset + 1], m_pixelArray[offset]);
         }
 
